11a05904d602a5042f161ee1d7fd31d901875f06
lei-clearsky <sportzhulei@gmail.com>
2015-01-15 11:07:10 -0500

    initial setup

---
+++ js/on-scroll-header.js
@@ -0,0 +1,36 @@
+var onScrollHeader = (function() {
+	var docElem = document.documentElement,
+		scroll = false,
+		headerOnScroll = $('.top-header-wrap');
+		towerOnScroll = $('.tower-logo-display');
+		changeHeaderOn = 90;
+
+	function initHeaderScroll() {
+		window.addEventListener( 'scroll', function( event ) {
+			if( !scroll ) {
+				scroll = true;
+				setTimeout( headerScroll, 250 );
+			}
+		}, false );
+	}
+
+	function headerScroll() {
+		var headerY = headerScrollY();
+	    if ( headerY >= changeHeaderOn && window.innerWidth > 960 ) {
+	        headerOnScroll.addClass("fix-top-header");
+	    towerOnScroll.addClass('toggle-display');
+	    }
+	    else {
+	        headerOnScroll.removeClass("fix-top-header");
+	    towerOnScroll.removeClass('toggle-display');
+	    }
+	    scroll = false;
+	}
+
+	function headerScrollY() {
+	        return window.pageYOffset || docElem.scrollTop;
+	}
+
+	initHeaderScroll();
+	
+})();
