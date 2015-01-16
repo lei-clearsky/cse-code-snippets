1ee433c6f03533dcf6b0ee44227cfa3192f13648
lei-clearsky <sportzhulei@gmail.com>
2015-01-16 15:08:12 -0500

    update formatting in index

--- mega-menu/megaMenu.js
+++ mega-menu/megaMenu.js
@@ -1,5 +1,6 @@
 var megaMenu=(function($) {
-  var b = $("#cbp-hrmenu > ul > li"),
+
+  var b = $("#cse-mega-menu > ul > li"),
       g = b.children("a.dropdown"),
       c = $("body"),
       d = -1;
@@ -12,27 +13,27 @@ var megaMenu=(function($) {
   function a(j) {
 
     if(d !== -1){
-      b.eq(d).removeClass("cbp-hropen")
+      b.eq(d).removeClass("cse-mega-menu-open")
     }
     
     var i = $(j.currentTarget).parent("li"),
         h = i.index();
     
     if(d === h) {
-      i.removeClass("cbp-hropen");
+      i.removeClass("cse-mega-menu-open");
       d = -1
     } else {
-      i.addClass("cbp-hropen");
+      i.addClass("cse-mega-menu-open");
       d = h;
-      c.off("click").on("click",e)
+      c.off("click").on("click", e)
     }
 
     return false;
   }
 
   function e(h){
-      b.eq(d).removeClass("cbp-hropen"); 
-      d=-1
+      b.eq(d).removeClass("cse-mega-menu-open"); 
+      d = -1
   }
     
   return{init: f}
